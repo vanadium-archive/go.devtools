16abad194ec1124f8753abbc6c88ab0f1a716711
Jiri Simsa <jsimsa@google.com>
2014-05-09 18:52:33 -0700

    TBR
    go/src/tools: inception of a command-line tool for development management
    
    The short-term plan is that this tool will be invoked indirectly by
    the new veyron setup script (see scripts/veyron in
    https://veyron-review.googlesource.com/#/c/2090/).
    
    The long-term plan is that this tool will grow to become the veyron
    build system, adding commands such as "build", "clean", "install",
    "test".
    
    Change-Id: Ibcef992c77cc72a0ef42e2173a37156858d95488

---
+++ veyron/impl/impl.go
@@ -0,0 +1,92 @@
+package impl
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"sort"
+
+	"veyron/lib/cmdline"
+)
+
+// Root returns a command that represents the root of the veyron tool.
+func Root() *cmdline.Command {
+	return &cmdline.Command{
+		Name:  "veyron",
+		Short: "Command-line tool for managing the veyron project",
+		Long: `
+The veyron tool facilitates interaction with the veyron project.
+In particular, it can be used to install different veyron profiles.
+`,
+		Children: []*cmdline.Command{cmdSetup},
+	}
+}
+
+var (
+	profiles = map[string]string{
+		"android":           "Android veyron development",
+		"cross-compilation": "cross-compilation for Linux/ARM",
+		"developer":         "core veyron development",
+	}
+)
+
+func profilesDescription() string {
+	result := `
+<profiles> is a list of profiles to set up. Currently, the veyron tool
+supports the following profiles:
+`
+	sortedProfiles := make([]string, 0)
+	maxLength := 0
+	for profile, _ := range profiles {
+		sortedProfiles = append(sortedProfiles, profile)
+		if len(profile) > maxLength {
+			maxLength = len(profile)
+		}
+	}
+	sort.Strings(sortedProfiles)
+	for _, profile := range sortedProfiles {
+		result += fmt.Sprintf("  %*s: %s\n", maxLength, profile, profiles[profile])
+	}
+	return result
+}
+
+// cmdSetup represents the 'setup' command of the veyron tool.
+var cmdSetup = &cmdline.Command{
+	Run:   runSetup,
+	Name:  "setup",
+	Short: "Set up the given veyron profiles",
+	Long: `
+To facilitate development across different platforms, veyron defines
+platform-independent profiles that map different platforms to a set
+of libraries and tools that can be used for a factor of veyron
+development. The "setup" command can be used to install the libraries
+and tools identified by the combination of the given profiles and
+the host platform.
+`,
+	ArgsName: "<profiles>",
+	ArgsLong: profilesDescription(),
+}
+
+func runSetup(cmd *cmdline.Command, args []string) error {
+	// Check that the profiles to be set up exist.
+	for _, arg := range args {
+		if _, ok := profiles[arg]; !ok {
+			cmd.Errorf("Unknown profile '%s'", arg)
+			return cmdline.ErrUsage
+		}
+	}
+	// Setup the profiles.
+	root := os.Getenv("VEYRON_ROOT")
+	script := path.Join(root, "environment/scripts/setup/machine/init.sh")
+	for _, arg := range args {
+		cmd := exec.Command(script, "-p", arg)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return errors.New("profile setup failed")
+		}
+	}
+	return nil
+}
---
+++ veyron/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"tools/veyron/impl"
+)
+
+func main() {
+	impl.Root().Main()
+}
