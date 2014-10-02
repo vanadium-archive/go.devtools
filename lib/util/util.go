219edcb84519935f88acd77086fc6ec5c3105dcd
Todd Wang <toddw@google.com>
2014-10-02 10:44:58 -0700

    tools/veyron: Set VDLPATH envvar, currently the same as GOPATH.
    I have a subsequent change which will cause vdl to start using
    VDLPATH rather than GOPATH.  These steps are prerequisites for
    our eventual goal of moving .vdl files out of the go repos.
    
    Also a minor refactoring of VeyronEnvironment to keep it a
    reasonable size.
    
    Change-Id: Ib04938ac56a56678aca8976109757c81eacdf95e

--- lib/util/util.go
+++ lib/util/util.go
@@ -40,6 +40,7 @@ func init() {
 		"CGO_CFLAGS",
 		"CGO_LDFLAGS",
 		"GOPATH",
+		"VDLPATH",
 	}
 	for _, v := range vars {
 		baseEnv[v] = os.Getenv(v)
@@ -176,13 +177,35 @@ func VeyronEnvironment() (map[string]string, error) {
 	if err := json.Unmarshal(configBytes, &config); err != nil {
 		return nil, fmt.Errorf("Unmarshal(%v) failed: %v", string(configBytes), err)
 	}
+	env := map[string]string{}
+	setEnvGoPath(env, root, config)
+	setEnvVdlPath(env, root, config)
+	if err := setEnvCgo(env, root); err != nil {
+		return nil, err
+	}
+	return env, nil
+}
+
+func setEnvGoPath(env map[string]string, root string, config configType) {
 	gopath := parseTokens(baseEnv["GOPATH"], ":")
 	// Append an entry to gopath for each veyron go repo.
 	for _, repo := range config.GoRepos {
 		gopath = append(gopath, filepath.Join(root, repo, "go"))
 	}
-	env := map[string]string{}
 	env["GOPATH"] = strings.Join(gopath, ":")
+}
+
+func setEnvVdlPath(env map[string]string, root string, config configType) {
+	vdlpath := parseTokens(baseEnv["VDLPATH"], ":")
+	// Append an entry to vdlpath for each veyron go repo.
+	// TODO(toddw): This logic will change when we pull vdl into a separate repo.
+	for _, repo := range config.GoRepos {
+		vdlpath = append(vdlpath, filepath.Join(root, repo, "go"))
+	}
+	env["VDLPATH"] = strings.Join(vdlpath, ":")
+}
+
+func setEnvCgo(env map[string]string, root string) error {
 	// Set the CGO_* variables for the veyron proximity component.
 	if runtime.GOOS == "linux" {
 		env["CGO_ENABLED"] = "1"
@@ -196,7 +219,7 @@ func VeyronEnvironment() (map[string]string, error) {
 		archCmd := exec.Command("uname", "-m")
 		arch, err := archCmd.Output()
 		if err != nil {
-			return nil, fmt.Errorf("failed to get host architecture: %v\n%v\n%s", err, strings.Join(archCmd.Args, " "))
+			return fmt.Errorf("failed to get host architecture: %v\n%v\n%s", err, strings.Join(archCmd.Args, " "))
 		}
 		cflags := parseTokens(baseEnv["CGO_CFLAGS"], " ")
 		ldflags := parseTokens(baseEnv["CGO_LDFLAGS"], " ")
@@ -204,7 +227,7 @@ func VeyronEnvironment() (map[string]string, error) {
 			dir := filepath.Join(root, "environment", "cout", lib, strings.TrimSpace(string(arch)))
 			if _, err := os.Stat(dir); err != nil {
 				if !os.IsNotExist(err) {
-					return nil, fmt.Errorf("Stat(%v) failed: %v", dir, err)
+					return fmt.Errorf("Stat(%v) failed: %v", dir, err)
 				}
 			} else {
 				if lib == "dbus-1.6.14" {
@@ -218,7 +241,7 @@ func VeyronEnvironment() (map[string]string, error) {
 		env["CGO_CFLAGS"] = strings.Join(cflags, " ")
 		env["CGO_LDFLAGS"] = strings.Join(ldflags, " ")
 	}
-	return env, nil
+	return nil
 }
 
 // VeyronRoot returns the root of the veyron universe.
