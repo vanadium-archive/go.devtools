39ecbb7aceb7df150c71a4eb528fc01d10f80eca
Todd Wang <toddw@google.com>
2014-12-16 10:41:16 -0800

    tools: Add gopkg, a simple tool to print go package info.
    Print information about go package(s).
    
    Example of printing all top-level information about the vdl
    package:
      veyron run gopkg veyron.io/veyron/veyron2/vdl
    
    Example of printing the names of the Test* funcs from the vdl
    package:
      veyron run gopkg -test -kind=func -name_re 'Test.*' -type_re 'func\(.*testing\.T\)' -noheader -notype veyron.io/veyron/veyron2/vdl
    
    Usage:
       gopkg [flags] <args>
    
    <args> is a list of arguments denoting a set of initial packages.
    It may take one of two forms:
    
    1. A list of *.go source files.
    
       All of the specified files are loaded, parsed and type-checked
       as a single package.  All the files must belong to the same directory.
    
    2. A list of import paths, each denoting a package.
    
       The package's directory is found relative to the $GOROOT and
       $GOPATH using similar logic to 'go build', and the *.go files in
       that directory are loaded, parsed and type-checked as a single
       package.
    
       In addition, all *_test.go files in the directory are then loaded
       and parsed.  Those files whose package declaration equals that of
       the non-*_test.go files are included in the primary package.  Test
       files whose package declaration ends with "_test" are type-checked
       as another package, the 'external' test package, so that a single
       import path may denote two packages.  (Whether this behaviour is
       enabled is tool-specific, and may depend on additional flags.)
    
       Due to current limitations in the type-checker, only the first
       import path of the command line will contribute any tests.
    
    A '--' argument terminates the list of packages.
    
    The gopkg flags are:
     -kind=const,var,func,type
       Print information for the specified kinds, in the order
       listed.
     -name_re=.*
       Filter out identifier names that don't match this regexp.
     -noheader=false
       Don't print headers.
     -noname=false
       Don't print identifier names.
     -notype=false
       Don't print type descriptions.
     -test=false
       Load test code (*_test.go) for packages.
     -type_re=.*
       Filter out type descriptions that don't match this regexp.
    
    Change-Id: Ibaec06e195f902202715f801c4aae63c7cfd96fb

---
+++ gopkg/doc.go
@@ -0,0 +1,60 @@
+// This file was auto-generated via go generate.
+// DO NOT UPDATE MANUALLY
+
+/*
+Print information about go package(s).
+
+Example of printing all top-level information about the vdl package:
+  veyron run gopkg veyron.io/veyron/veyron2/vdl
+
+Example of printing the names of all Test* funcs from the vdl package:
+  veyron run gopkg -test -kind=func -name_re 'Test.*' -type_re 'func\(.*testing\.T\)' -noheader -notype veyron.io/veyron/veyron2/vdl
+
+Usage:
+   gopkg [flags] <args>
+
+<args> is a list of arguments denoting a set of initial packages. It may take
+one of two forms:
+
+1. A list of *.go source files.
+
+   All of the specified files are loaded, parsed and type-checked
+   as a single package.  All the files must belong to the same directory.
+
+2. A list of import paths, each denoting a package.
+
+   The package's directory is found relative to the $GOROOT and
+   $GOPATH using similar logic to 'go build', and the *.go files in
+   that directory are loaded, parsed and type-checked as a single
+   package.
+
+   In addition, all *_test.go files in the directory are then loaded
+   and parsed.  Those files whose package declaration equals that of
+   the non-*_test.go files are included in the primary package.  Test
+   files whose package declaration ends with "_test" are type-checked
+   as another package, the 'external' test package, so that a single
+   import path may denote two packages.  (Whether this behaviour is
+   enabled is tool-specific, and may depend on additional flags.)
+
+   Due to current limitations in the type-checker, only the first
+   import path of the command line will contribute any tests.
+
+A '--' argument terminates the list of packages.
+
+The gopkg flags are:
+ -kind=const,var,func,type
+   Print information for the specified kinds, in the order listed.
+ -name_re=.*
+   Filter out identifier names that don't match this regexp.
+ -noheader=false
+   Don't print headers.
+ -noname=false
+   Don't print identifier names.
+ -notype=false
+   Don't print type descriptions.
+ -test=false
+   Load test code (*_test.go) for packages.
+ -type_re=.*
+   Filter out type descriptions that don't match this regexp.
+*/
+package main
---
+++ gopkg/gopkg.go
@@ -0,0 +1,237 @@
+// The following enables go generate to generate the doc.go file.
+//go:generate go run $VEYRON_ROOT/veyron/go/src/veyron.io/lib/cmdline/testdata/gendoc.go . -help
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"code.google.com/p/go.tools/go/loader"
+	"code.google.com/p/go.tools/go/types"
+	"veyron.io/lib/cmdline"
+)
+
+func main() {
+	cmdGoPkg.Main()
+}
+
+var cmdGoPkg = &cmdline.Command{
+	Run:   runGoPkg,
+	Name:  "gopkg",
+	Short: "Print information about go package(s)",
+	Long: `
+Print information about go package(s).
+
+Example of printing all top-level information about the vdl package:
+  veyron run gopkg veyron.io/veyron/veyron2/vdl
+
+Example of printing the names of all Test* funcs from the vdl package:
+  veyron run gopkg -test -kind=func -name_re 'Test.*' -type_re 'func\(.*testing\.T\)' -noheader -notype veyron.io/veyron/veyron2/vdl
+`,
+	ArgsName: "<args>",
+	ArgsLong: loader.FromArgsUsage,
+}
+
+var (
+	flagTest     bool
+	flagNoHeader bool
+	flagNoName   bool
+	flagNoType   bool
+	flagKind     Kinds = KindAll
+	flagNameRE   string
+	flagTypeRE   string
+)
+
+func init() {
+	cmdGoPkg.Flags.BoolVar(&flagTest, "test", false, "Load test code (*_test.go) for packages.")
+	cmdGoPkg.Flags.BoolVar(&flagNoHeader, "noheader", false, "Don't print headers.")
+	cmdGoPkg.Flags.BoolVar(&flagNoName, "noname", false, "Don't print identifier names.")
+	cmdGoPkg.Flags.BoolVar(&flagNoType, "notype", false, "Don't print type descriptions.")
+	cmdGoPkg.Flags.Var(&flagKind, "kind", "Print information for the specified kinds, in the order listed.")
+	cmdGoPkg.Flags.StringVar(&flagNameRE, "name_re", ".*", "Filter out identifier names that don't match this regexp.")
+	cmdGoPkg.Flags.StringVar(&flagTypeRE, "type_re", ".*", "Filter out type descriptions that don't match this regexp.")
+}
+
+func parseRegexp(expr string) (*regexp.Regexp, error) {
+	// Make sure the regexp performs a full match against the target string.
+	expr = strings.TrimSpace(expr)
+	if !strings.HasPrefix(expr, "^") {
+		expr = "^" + expr
+	}
+	if !strings.HasSuffix(expr, "$") {
+		expr = expr + "$"
+	}
+	return regexp.Compile(expr)
+}
+
+func runGoPkg(cmd *cmdline.Command, args []string) error {
+	// Parse flags.
+	nameRE, err := parseRegexp(flagNameRE)
+	if err != nil {
+		return err
+	}
+	typeRE, err := parseRegexp(flagTypeRE)
+	if err != nil {
+		return err
+	}
+	// Load packages specified in args.
+	config := loader.Config{
+		SourceImports:       false,
+		TypeCheckFuncBodies: func(string) bool { return false },
+	}
+	args, err = config.FromArgs(args, flagTest)
+	if err != nil {
+		return cmd.UsageErrorf("failed to parse args: %v", err)
+	}
+	if len(args) != 0 {
+		return cmd.UsageErrorf("unrecognized args %q", args)
+	}
+	prog, err := config.Load()
+	if err != nil {
+		return err
+	}
+	// Print information for each loaded package.
+	for _, pkginfo := range prog.InitialPackages() {
+		pkg := pkginfo.Pkg
+		if !flagNoHeader {
+			fmt.Fprintf(cmd.Stdout(), "%s (%s)\n", pkg.Path(), pkg.Name())
+		}
+		scope := pkg.Scope()
+		data := make(map[Kind][]NameType)
+		for _, name := range scope.Names() {
+			if !nameRE.MatchString(name) {
+				continue
+			}
+			kind, nt := NameTypeFromObject(scope.Lookup(name))
+			if !typeRE.MatchString(nt.Type) {
+				continue
+			}
+			data[kind] = append(data[kind], nt)
+		}
+		for _, kind := range flagKind {
+			if !flagNoHeader {
+				fmt.Fprintf(cmd.Stdout(), "%ss\n", strings.Title(kind.String()))
+			}
+			for _, nt := range data[kind] {
+				var line string
+				if !flagNoName {
+					line += " " + nt.Name
+				}
+				if !flagNoType {
+					line += " " + nt.Type
+				}
+				line = strings.TrimSpace(line)
+				if line != "" {
+					fmt.Fprintf(cmd.Stdout(), "  %s\n", line)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// NameType holds the name and type of a top-level declaration.
+type NameType struct {
+	Name string
+	Type string
+}
+
+func NameTypeFromObject(obj types.Object) (Kind, NameType) {
+	var kind Kind
+	switch obj.(type) {
+	case *types.Const:
+		kind = Const
+	case *types.Var:
+		kind = Var
+	case *types.Func:
+		kind = Func
+	case *types.TypeName:
+		kind = Type
+	default:
+		panic(fmt.Errorf("unhandled types.Object %#v", obj))
+	}
+	return kind, NameType{obj.Name(), obj.Type().String()}
+}
+
+// Kind describes the kind of a top-level declaration, usable as a flag.
+type Kind int
+
+// Kinds holds a slice of Kind, usable as a flag.
+type Kinds []Kind
+
+const (
+	Const Kind = iota // Top-level const declaration.
+	Var               // Top-level var declaration.
+	Func              // Top-level func declaration.
+	Type              // Top-level type declaration.
+)
+
+var KindAll = Kinds{Const, Var, Func, Type}
+
+func KindFromString(s string) (k Kind, err error) {
+	err = k.Set(s)
+	return
+}
+
+func (k *Kind) Set(s string) error {
+	switch s {
+	case "const":
+		*k = Const
+		return nil
+	case "var":
+		*k = Var
+		return nil
+	case "func":
+		*k = Func
+		return nil
+	case "type":
+		*k = Type
+		return nil
+	default:
+		*k = -1
+		return fmt.Errorf("unknown Kind %q", s)
+	}
+}
+
+func (k Kind) String() string {
+	switch k {
+	case Const:
+		return "const"
+	case Var:
+		return "var"
+	case Func:
+		return "func"
+	case Type:
+		return "type"
+	default:
+		return fmt.Sprintf("Kind(%d)", k)
+	}
+}
+
+func (kinds *Kinds) Set(s string) error {
+	*kinds = nil
+	seen := make(map[Kind]bool)
+	for _, kindstr := range strings.Split(s, ",") {
+		if kindstr == "" {
+			continue
+		}
+		k, err := KindFromString(kindstr)
+		if err != nil {
+			return err
+		}
+		if !seen[k] {
+			seen[k] = true
+			*kinds = append(*kinds, k)
+		}
+	}
+	return nil
+}
+
+func (kinds Kinds) String() string {
+	var strs []string
+	for _, k := range kinds {
+		strs = append(strs, k.String())
+	}
+	return strings.Join(strs, ",")
+}
