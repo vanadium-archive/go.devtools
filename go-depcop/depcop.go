4bca3e35b966bb4b5e278a415c8f6bb86bbe8659
James Ring <sjr@jdns.org>
2015-01-27 17:08:53 -0800

    Revert "veyron.io/tools: fix flaky test"
    This reverts commit 2716fb65c0d49b918f7f244f4a0338148f71f756. The
    underlying bug is fixed in Go1.4.1 (which we now use), so the original
    CL is no longer required.
    
    Change-Id: I7fb0ff8fba0e117b93c78de8728eedd8b9ec6105

--- go-depcop/depcop.go
+++ go-depcop/depcop.go
@@ -87,7 +87,7 @@ func (r dependencyRule) enforce(p *build.Package) (dependencyPolicyAction, error
 	return undecidedPolicyAction, nil
 }
 
-func collectDirs(root, suffix string, dirs map[string]bool) error {
+func collectDirs(root, suffix string, dirs map[string]struct{}) error {
 	path := filepath.Join(root, suffix)
 	fis, err := ioutil.ReadDir(path)
 	if err != nil {
@@ -96,28 +96,28 @@ func collectDirs(root, suffix string, dirs map[string]bool) error {
 	for _, fi := range fis {
 		if fi.IsDir() {
 			suffix2 := filepath.Join(suffix, fi.Name())
-			dirs[suffix2] = true
+			dirs[suffix2] = struct{}{}
 			collectDirs(root, suffix2, dirs)
 		}
 	}
 	return nil
 }
 
-func computeIncomingDependencies() (map[string]map[string]bool, error) {
+func computeIncomingDependencies() (map[string]map[string]struct{}, error) {
 	gopath := os.Getenv("GOPATH")
 	if gopath == "" {
 		return nil, fmt.Errorf("GOPATH is not set")
 	}
 	dirs := strings.Split(gopath, ":")
-	allDirs := map[string]bool{}
+	allDirs := map[string]struct{}{}
 	for _, dir := range dirs {
 		if err := collectDirs(filepath.Join(dir, "src"), "", allDirs); err != nil {
 			return nil, err
 		}
 	}
-	allDeps := map[string]map[string]bool{}
+	allDeps := map[string]map[string]struct{}{}
 	for dir, _ := range allDirs {
-		allDeps[dir] = map[string]bool{}
+		allDeps[dir] = map[string]struct{}{}
 	}
 	for dir, _ := range allDirs {
 		mode := build.ImportMode(0)
@@ -131,7 +131,7 @@ func computeIncomingDependencies() (map[string]map[string]bool, error) {
 		}
 		for _, dep := range imports {
 			if deps, ok := allDeps[dep]; ok {
-				deps[dir] = true
+				deps[dir] = struct{}{}
 			}
 		}
 	}
