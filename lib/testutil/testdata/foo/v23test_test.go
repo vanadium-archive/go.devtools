0c39677d2a98a2453bb8f0bfb6e92a4935750a6e
Cosmos Nicolaou <cnicolaou@google.com>
2015-03-10 07:48:29 -0700

    modules usability improvements: https://vanadium-review.googlesource.com/#/c/7060/ MultiPart: 2/5 Change-Id: Ia9277a197b6b324334b1399543b6c7bd3a641fb5

--- lib/testutil/testdata/foo/v23test_test.go
+++ lib/testutil/testdata/foo/v23test_test.go
@@ -1,14 +1,8 @@
 package foo_test
 
 import (
-	"testing"
-
-	"v.io/x/ref/lib/modules"
 	"v.io/x/ref/lib/testutil/v23tests"
 	_ "v.io/x/ref/profiles"
 )
 
-func TestHelperProcess(t *testing.T) {
-	modules.DispatchInTest()
-}
 func V23Test(i *v23tests.T) {}
