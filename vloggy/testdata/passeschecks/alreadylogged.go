35c567dee650d91b0e0ac79bb6482afc344ee9d6
Jiri Simsa <jsimsa@google.com>
2015-03-24 22:39:33 -0700

    devtools: adding copyright headers and licensing files
    Change-Id: Iaaf23e1b7a79926bcf12dc04d63bfd396144f206

--- vloggy/testdata/passeschecks/alreadylogged.go
+++ vloggy/testdata/passeschecks/alreadylogged.go
@@ -1,3 +1,7 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // passeschecks should pass all the log checks, as it includes
 // all the necessary logging constructs.
 package passeschecks
