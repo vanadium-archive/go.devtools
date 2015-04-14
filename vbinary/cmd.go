9ae57a3830816bcfbf77a1353277e01423c4b28f
Suharsh Sivakumar <suharshs@google.com>
2015-04-14 15:06:16 -0700

    devtools/vbinary: Introducing the vbinary tool.
    'vbinary list' lists the available binaries to download.
    
    'vbinary download' downloads a set of binaries for the specified date.
    It will download the lastest one if no date is specified.
    
    Change-Id: Ifcbfdf2972bd46cfe6adb35594af91aa720fcad7

---
+++ vbinary/cmd.go
@@ -0,0 +1,318 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/googleapi"
+	"google.golang.org/api/storage/v1"
+
+	"v.io/x/devtools/internal/collect"
+	"v.io/x/devtools/internal/tool"
+	"v.io/x/lib/cmdline"
+)
+
+var (
+	colorFlag   bool
+	dryRunFlag  bool
+	verboseFlag bool
+
+	datePrefixFlag string
+	keyFileFlag    string
+	outputDirFlag  string
+)
+
+const (
+	binariesBucketName = "vanadium-binaries"
+)
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	cmdRoot.Flags.BoolVar(&verboseFlag, "v", false, "Print verbose output.")
+	cmdRoot.Flags.BoolVar(&dryRunFlag, "n", false, "Show what commands will run but do not execute them.")
+	cmdRoot.Flags.BoolVar(&colorFlag, "color", true, "Use color to format output.")
+
+	cmdRoot.Flags.StringVar(&keyFileFlag, "key-file", "", "Google Developers service account JSON key file.")
+	cmdRoot.Flags.StringVar(&datePrefixFlag, "date-prefix", "", "Date prefix to match daily build timestamps. Must be a prefix of YYYY-MM-DD.")
+	cmdDownload.Flags.StringVar(&outputDirFlag, "output-dir", "", "Directory for storing downloaded binaries.")
+}
+
+// root returns a command that represents the root of the vbinary tool.
+func root() *cmdline.Command {
+	return cmdRoot
+}
+
+// cmdRoot represents the "vbinary" command.
+var cmdRoot = &cmdline.Command{
+	Name:  "vbinary",
+	Short: "Access daily builds of Vanadium binaries",
+	Long: `
+Daily builds of Vanadium binaries are stored in a Google Storage
+bucket. This command facilitates retrieval of these binaries, for
+instance for the purpose of regression testing.
+`,
+	Children: []*cmdline.Command{cmdList, cmdDownload},
+}
+
+// cmdList represents the "vbinary list" command.
+var cmdList = &cmdline.Command{
+	Run:   runList,
+	Name:  "list",
+	Short: "List existing daily builds of Vanadium binaries",
+	Long: `
+List existing daily builds of Vanadium binaries. The displayed dates can be
+limited with the --date-prefix flag.
+`,
+}
+
+func runList(command *cmdline.Command, _ []string) error {
+	ctx := tool.NewContextFromCommand(command, tool.ContextOpts{
+		Color:   &colorFlag,
+		DryRun:  &dryRunFlag,
+		Verbose: &verboseFlag,
+	})
+	client, err := createClient(ctx)
+	if err != nil {
+		return err
+	}
+	service, err := storage.New(client)
+	if err != nil {
+		return err
+	}
+	binaries, err := binarySnapshots(service)
+	if err != nil {
+		return err
+	}
+	for _, name := range binaries {
+		fmt.Fprintf(ctx.Stdout(), "%s\n", name)
+	}
+	return nil
+}
+
+// cmdDownload represents the "vbinary download" command.
+var cmdDownload = &cmdline.Command{
+	Run:   runDownload,
+	Name:  "download",
+	Short: "Download an existing daily build of Vanadium binaries",
+	Long: `
+Download an existing daily build of Vanadium binaries. The latest snapshot within
+the --date-prefix range will be downloaded. If no --date-prefix flag is provided,
+the overall latest snapshot will be downloaded.`,
+}
+
+func runDownload(command *cmdline.Command, args []string) error {
+	ctx := tool.NewContextFromCommand(command, tool.ContextOpts{
+		Color:   &colorFlag,
+		DryRun:  &dryRunFlag,
+		Verbose: &verboseFlag,
+	})
+	client, err := createClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	binaries, timestamp, err := latestBinaries(client)
+	if err != nil {
+		return err
+	}
+	if len(outputDirFlag) == 0 {
+		outputDirFlag = fmt.Sprintf("./v23_%s_%s_%s", runtime.GOOS, runtime.GOARCH, timestamp)
+	}
+	if err := ctx.Run().MkdirAll(outputDirFlag, 0755); err != nil {
+		return fmt.Errorf("Error while creating directory %s: %s", outputDirFlag, err)
+	}
+
+	numBinaries := len(binaries)
+	downloadBinaries := func() error {
+		errChan := make(chan error, numBinaries)
+		for _, name := range binaries {
+			go downloadBinary(ctx, client, name, errChan)
+		}
+		gotError := false
+		for i := 0; i < numBinaries; i++ {
+			if err := <-errChan; err != nil {
+				fmt.Fprintf(ctx.Stderr(), "failed to download binary: %v", err)
+				gotError = true
+			}
+		}
+		if gotError {
+			if err := ctx.Run().RemoveAll(outputDirFlag); err != nil {
+				fmt.Fprintf(ctx.Stderr(), "failed to remove %s: %v", outputDirFlag, err)
+			}
+			return fmt.Errorf("operation failed")
+		}
+		return nil
+	}
+	return ctx.Run().Function(downloadBinaries, fmt.Sprintf("Downloading binaries to %s", outputDirFlag))
+}
+
+// latestBinaries returns the binaries of the latest snapshot whose timestamp
+// matches the datePrefixFlag, along with the matching timestamp.
+func latestBinaries(client *http.Client) ([]string, string, error) {
+	service, err := storage.New(client)
+	if err != nil {
+		return nil, "", err
+	}
+	timestamp, err := latestTimestamp(client, service)
+	if err != nil {
+		return nil, "", err
+	}
+	binaryPrefix := fmt.Sprintf("%s_%s/%s", runtime.GOOS, runtime.GOARCH, timestamp)
+	res, err := service.Objects.List(binariesBucketName).Fields("nextPageToken", "items/name").Prefix(binaryPrefix).Do()
+	if err != nil {
+		return nil, "", err
+	}
+	objs := res.Items
+	for res.NextPageToken != "" {
+		res, err = service.Objects.List(binariesBucketName).PageToken(res.NextPageToken).Do()
+		if err != nil {
+			return nil, "", err
+		}
+		objs = append(objs, res.Items...)
+	}
+	if len(objs) == 0 {
+		return nil, "", fmt.Errorf("no binaries found (OS: %s, Arch: %s, Date: %s)", runtime.GOOS, runtime.GOARCH, timestamp)
+	}
+	ret := make([]string, len(objs))
+	for i, obj := range objs {
+		ret[i] = obj.Name
+	}
+	return ret, timestamp, nil
+}
+
+// latestTimestamp returns the time the latest snapshot within the
+// date-prefix range.
+func latestTimestamp(client *http.Client, service *storage.Service) (string, error) {
+	// If no datePrefixFlag is provided, we just want to get the latest snapshot.
+	if datePrefixFlag == "" {
+		latestFile := fmt.Sprintf("%s_%s/%s", runtime.GOOS, runtime.GOARCH, "latest")
+		b, err := downloadFileBytes(client, latestFile)
+		if err != nil {
+			return "", err
+		}
+		return string(b), nil
+	}
+	// Otherwise, we get the snapshots that match datePrefixFlag and choose the latest.
+	snapshots, err := binarySnapshots(service)
+	if err != nil {
+		return "", err
+	}
+	const layout = "2006-01-02T15:04:05-07:00"
+	var latest string
+	var latestTime time.Time
+	for _, name := range snapshots {
+		spl := strings.Split(name, "/")
+		timestamp := spl[1]
+		t, err := time.Parse(layout, timestamp)
+		if err != nil {
+			return "", err
+		}
+		if t.After(latestTime) {
+			latest = timestamp
+			latestTime = t
+		}
+	}
+	if latest == "" {
+		return "", fmt.Errorf("no binaries exist")
+	}
+	return latest, nil
+}
+
+func binarySnapshots(service *storage.Service) ([]string, error) {
+	// TODO(suharshs,jsimsa): Snapshots have a .done file written to them if they
+	// were completely successful. We need to have this code only display the snapshot
+	// if the .done file is present.
+	binaryPrefix := fmt.Sprintf("%s_%s/%s", runtime.GOOS, runtime.GOARCH, datePrefixFlag)
+	// We delimit results by the end of the timestamp string so that we don't display
+	// duplicate timestamps for each binary.
+	res, err := service.Objects.List(binariesBucketName).Fields("nextPageToken", "prefixes").Prefix(binaryPrefix).Delimiter(":00/").Do()
+	if err != nil {
+		return nil, err
+	}
+	snapshots := res.Prefixes
+	for res.NextPageToken != "" {
+		res, err = service.Objects.List(binariesBucketName).PageToken(res.NextPageToken).Do()
+		if err != nil {
+			return nil, err
+		}
+		snapshots = append(snapshots, res.Prefixes...)
+	}
+	if len(snapshots) == 0 {
+		return nil, fmt.Errorf("no snapshots found (OS: %s, Arch: %s, Date: %s)", runtime.GOOS, runtime.GOARCH, datePrefixFlag)
+	}
+	return snapshots, nil
+}
+
+func createClient(ctx *tool.Context) (*http.Client, error) {
+	oauthHttpClient := &http.Client{}
+	if len(keyFileFlag) > 0 {
+		data, err := ctx.Run().ReadFile(keyFileFlag)
+		if err != nil {
+			return nil, err
+		}
+		conf, err := google.JWTConfigFromJSON(data, storage.CloudPlatformScope)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create JWT config file: %v", err)
+		}
+		oauthHttpClient = conf.Client(oauth2.NoContext)
+	}
+	return oauthHttpClient, nil
+}
+
+func downloadBinary(ctx *tool.Context, client *http.Client, binaryPath string, errChan chan<- error) {
+	helper := func() error {
+		b, err := downloadFileBytes(client, binaryPath)
+		if err != nil {
+			return fmt.Errorf("failed to download file %v: %v", binaryPath, err)
+		}
+		fileName := filepath.Join(outputDirFlag, path.Base(binaryPath))
+		if err := ctx.Run().WriteFile(fileName, b, 0755); err != nil {
+			return fmt.Errorf("failed to write file: %v", err)
+		}
+		return nil
+	}
+	errChan <- helper()
+}
+
+func downloadFileBytes(client *http.Client, filePath string) (b []byte, e error) {
+	// This roundabout request is required because of the issue detailed here:
+	// https://plus.sandbox.google.com/+IanRose/posts/Tzw3QZqEQZk
+	// and here:
+	// https://groups.google.com/forum/#!msg/Golang-nuts/juguXl-ss2Q/oOVFvHYqoSgJ.
+	urls := "https://www.googleapis.com/download/storage/v1/b/{bucket}/o/{object}?alt=media"
+	req, err := http.NewRequest("GET", urls, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request to %s: %v\n", urls, err)
+	}
+	req.URL.Path = strings.Replace(req.URL.Path, "{bucket}", url.QueryEscape(binariesBucketName), 1)
+	req.URL.Path = strings.Replace(req.URL.Path, "{object}", url.QueryEscape(filePath), 1)
+	googleapi.SetOpaque(req.URL)
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to download %v: %v\n", req.URL.RequestURI(), err)
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("got StatusCode %v for download %v", req.URL.RequestURI(), res.StatusCode)
+	}
+	defer collect.Error(func() error { return res.Body.Close() }, &e)
+
+	var buf bytes.Buffer
+	if _, err := buf.ReadFrom(res.Body); err != nil {
+		return nil, fmt.Errorf("failed to read response body: %v", err)
+	}
+	return buf.Bytes(), nil
+}
---
+++ vbinary/doc.go
@@ -0,0 +1,78 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file was auto-generated via go generate.
+// DO NOT UPDATE MANUALLY
+
+/*
+Daily builds of Vanadium binaries are stored in a Google Storage bucket. This
+command facilitates retrieval of these binaries, for instance for the purpose of
+regression testing.
+
+Usage:
+   vbinary [flags] <command>
+
+The vbinary commands are:
+   list        List existing daily builds of Vanadium binaries
+   download    Download an existing daily build of Vanadium binaries
+   help        Display help for commands or topics
+Run "vbinary help [command]" for command usage.
+
+The vbinary flags are:
+ -color=true
+   Use color to format output.
+ -date-prefix=
+   Date prefix to match daily build timestamps. Must be a prefix of YYYY-MM-DD.
+ -key-file=
+   Google Developers service account JSON key file.
+ -n=false
+   Show what commands will run but do not execute them.
+ -v=false
+   Print verbose output.
+
+Vbinary List
+
+List existing daily builds of Vanadium binaries. The displayed dates can be
+limited with the --date-prefix flag.
+
+Usage:
+   vbinary list
+
+Vbinary Download
+
+Download an existing daily build of Vanadium binaries. The latest snapshot
+within the --date-prefix range will be downloaded. If no --date-prefix flag is
+provided, the overall latest snapshot will be downloaded.
+
+Usage:
+   vbinary download [flags]
+
+The vbinary download flags are:
+ -output-dir=
+   Directory for storing downloaded binaries.
+
+Vbinary Help
+
+Help with no args displays the usage of the parent command.
+
+Help with args displays the usage of the specified sub-command or help topic.
+
+"help ..." recursively displays help for all commands and topics.
+
+The output is formatted to a target width in runes.  The target width is
+determined by checking the environment variable CMDLINE_WIDTH, falling back on
+the terminal width from the OS, falling back on 80 chars.  By setting
+CMDLINE_WIDTH=x, if x > 0 the width is x, if x < 0 the width is unlimited, and
+if x == 0 or is unset one of the fallbacks is used.
+
+Usage:
+   vbinary help [flags] [command/topic ...]
+
+[command/topic ...] optionally identifies a specific sub-command or help topic.
+
+The vbinary help flags are:
+ -style=default
+   The formatting style for help output, either "default" or "godoc".
+*/
+package main
