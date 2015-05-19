b6c094a4199573eee63ec62f1f0237bada8b0ed8
Jiri Simsa <jsimsa@google.com>
2015-05-18 20:15:49 -0700

    devtools/depcop: renaming go-depcop to godepcop and logcop to gologcop (part 3)
    Because we do not have a mechanism to pass a modified manifest to
    presubmit, renaming of developer tools requires three steps.
    
    1) create godepcop as a copy of go-depcop and gologcop as a copy of
       logcop (this CL)
    
    2) add godepcop and gologcop to relevant manifests and replace uses of
       go-depcop and logcop in v23 and presubmit with godepcop and
       gologcop respectively
    
    3) remove go-depcop and logcop from both relevant manifests and the
       devtools project
    
    Change-Id: Ifa1ccdcd2ee24206d6ef05fb012319cad2512c07

--- logcop/testdata/failschecks/test4/test4b.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2015 The Vanadium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package test4
-
-func (Type1) Method2(int) {
-	//nologcall
-}
