aa01100749a8b7d3f28bae3ce737f3f12a366f7a
Jing Jin <jingjin@google.com>
2015-08-18 23:47:51 +0000

    TBR: Revert "TBR: update jenkins url"
    This reverts commit 7b4bcc40836e1b8844722769d48afb617bdbb87f.
    
    Change-Id: I7ca41f4171e6f0423123221df6d1cc1e4ab765b1

--- presubmit/common_test.go
+++ presubmit/common_test.go
@@ -18,7 +18,7 @@ func TestGenStartPresubmitBuildLink(t *testing.T) {
 	strTests := "vanadium-go-test vanadium-go-race"
 
 	got := genStartPresubmitBuildLink(strRefs, strProjects, strTests)
-	want := "http://veyron-linux1.mtv.corp.google.com:8001/jenkins/job/vanadium-presubmit-test/buildWithParameters?REFS=refs%2Fchanges%2F12%2F1012%2F1%3Arefs%2Fchanges%2F13%2F1013%2F1&PROJECTS=release.go.core%3Arelease.go.tools&TESTS=vanadium-go-test+vanadium-go-race"
+	want := "https://veyron.corp.google.com/jenkins/job/vanadium-presubmit-test/buildWithParameters?REFS=refs%2Fchanges%2F12%2F1012%2F1%3Arefs%2Fchanges%2F13%2F1013%2F1&PROJECTS=release.go.core%3Arelease.go.tools&TESTS=vanadium-go-test+vanadium-go-race"
 	if want != got {
 		t.Fatalf("\nwant:\n%s\n\ngot:\n%s\n", want, got)
 	}
