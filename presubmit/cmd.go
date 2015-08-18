73240d5127c9b8c2e0d979e0673cd2101e73c7ed
Jiri Simsa <jsimsa@google.com>
2015-08-18 16:09:15 -0700

    devtools/cmd: creating stubs for v23 binary-based subcommands
    Change-Id: I448ecd1bbf6b11acb59587b3509134f216fec0bb

--- presubmit/cmd.go
+++ presubmit/cmd.go
@@ -23,7 +23,7 @@ const (
 	defaultGerritBaseUrl    = "https://vanadium-review.googlesource.com"
 	defaultPresubmitTestJob = "vanadium-presubmit-test"
 	defaultQueryString      = "(status:open -project:experimental)"
-	jenkinsBaseJobUrl       = "http://veyron-linux1.mtv.corp.google.com:8001/jenkins/job"
+	jenkinsBaseJobUrl       = "https://veyron.corp.google.com/jenkins/job"
 	outputPrefix            = "[VANADIUM PRESUBMIT]"
 )
 
