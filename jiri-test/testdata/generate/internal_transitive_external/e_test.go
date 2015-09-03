68077f4bf712df382320feabd905afb8b11c3163
Jing Jin <jingjin@google.com>
2015-09-02 18:36:54 -0700

    devtools/jiri-test: fix testdata for generate_test
    Adam's email was pretty useful:
    
    https://groups.google.com/a/google.com/forum/#!topic/veyron-team/Yw9wef49QlY
    
    Change-Id: I56491877b90b1061940068689a5eb2e299d5f439

--- jiri-test/testdata/generate/internal_transitive_external/e_test.go
+++ jiri-test/testdata/generate/internal_transitive_external/e_test.go
@@ -2,15 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package internal_transitive_external_test
+package internal_transitive_external
 
 import (
 	"testing"
 
-	"v.io/x/devtools/jiri-test/testdata/generate/internal_transitive_external"
 	_ "v.io/x/ref/runtime/factories/generic"
 )
 
 func TestModulesExternal(t *testing.T) {
-	internal_transitive_external.Module(t)
+	Module(t)
 }
