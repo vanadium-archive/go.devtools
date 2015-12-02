0545b47c253afcee427b580b0f0bb0694177b394
Ross Wang <rosswang@google.com>
2015-12-02 13:13:32 -0800

    Add Baku Toolkit Java presubmits
    Change-Id: I16252fe918512b2cc05a6d02144e957278f8ba2c

--- jiri-test/internal/test/java.go
+++ jiri-test/internal/test/java.go
@@ -14,8 +14,8 @@ import (
 	"v.io/x/lib/envvar"
 )
 
-// vanadiumJavaTest runs all Java tests.
-func vanadiumJavaTest(jirix *jiri.X, testName string, opts ...Opt) (_ *test.Result, e error) {
+// runJavaTest includes common run logic for Java tests.
+func runJavaTest(jirix *jiri.X, testName string, cwd []string, task string) (_ *test.Result, e error) {
 	// Initialize the test.
 	cleanup, err := initTest(jirix, testName, []string{"java"})
 	if err != nil {
@@ -32,14 +32,19 @@ func vanadiumJavaTest(jirix *jiri.X, testName string, opts ...Opt) (_ *test.Resu
 	env := envvar.VarsFromOS()
 	env.Set("JAVA_HOME", ch.Get("JAVA_HOME"))
 	// Run tests.
-	javaDir := filepath.Join(ch.Root(), "release", "java")
+	javaDir := filepath.Join(append([]string{ch.Root()}, cwd...)...)
 	if err := jirix.Run().Chdir(javaDir); err != nil {
 		return nil, err
 	}
 	runOpts := jirix.Run().Opts()
 	runOpts.Env = env.ToMap()
-	if err := jirix.Run().CommandWithOpts(runOpts, filepath.Join(javaDir, "gradlew"), "--info", ":lib:check"); err != nil {
+	if err := jirix.Run().CommandWithOpts(runOpts, filepath.Join(javaDir, "gradlew"), "--info", task); err != nil {
 		return nil, err
 	}
 	return &test.Result{Status: test.Passed}, nil
+}
+
+// vanadiumJavaTest runs all Java tests.
+func vanadiumJavaTest(jirix *jiri.X, testName string, opts ...Opt) (_ *test.Result, e error) {
+	return runJavaTest(jirix, testName, []string{"release", "java"}, ":lib:check")
 }
