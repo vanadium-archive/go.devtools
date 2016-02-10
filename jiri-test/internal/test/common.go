91208cbc6e464115bb8df8c10cd3c93d37fd8eb8
Nicolas Lacasse <nlacasse@google.com>
2016-02-10 09:42:39 -0800

    jiri-test: Disable profile cleanup before tests.
    Change-Id: Ifeebc4603831810d58b7fee348ce4792f81752ab

--- jiri-test/internal/test/common.go
+++ jiri-test/internal/test/common.go
@@ -81,13 +81,13 @@ func regTestBinDirPath() string {
 
 // initTest carries out the initial actions for the given test.
 func initTest(jirix *jiri.X, testName string, profileNames []string, opts ...initTestOpt) (func() error, error) {
-	return initTestImpl(jirix, true, true, testName, profileNames, "", opts...)
+	return initTestImpl(jirix, false, true, testName, profileNames, "", opts...)
 }
 
 // initTestForTarget carries out the initial actions for the given test using
 // a specific profile Target..
 func initTestForTarget(jirix *jiri.X, testName string, profileNames []string, target string, opts ...initTestOpt) (func() error, error) {
-	return initTestImpl(jirix, true, true, testName, profileNames, target, opts...)
+	return initTestImpl(jirix, false, true, testName, profileNames, target, opts...)
 }
 
 func initTestImpl(jirix *jiri.X, needCleanup, printProfiles bool, testName string, profileNames []string, target string, opts ...initTestOpt) (func() error, error) {
