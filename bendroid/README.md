29312eeb1c8685a004832874766a4b219737de9a
Asim Shankar <ashankar@google.com>
2016-03-07 14:23:46 -0800

    bendroid: Re-implement in a much simpler fashion.
    Matt and I realized that shared libraries built for Android by the Go
    toolchain are executable. Furthermore, we remembered the -exec flag
    to "go test". Which means bendroid can be simplified considerably
    and now it is a much smaller program that is a prettier shell script.
    
    The only depdendency is on the 'adb' tool. No gradle or Java compilation
    required.
    
    Change-Id: I9a2293deba187cec903100348160ba54c5fd2108

--- bendroid/README.md
+++ bendroid/README.md
@@ -1,28 +1,4 @@
 # bendroid: Benchmark and run tests on Android
 
-## Prerequisites & installation
-
-- [Android Studio](http://developer.android.com/sdk/index.html) (which should make `adb` and `gradle` available in your path)
-- [Android NDK Toolchain](http://developer.android.com/ndk/index.html)
-- [Go](https://golang.org) version 1.6 or greater
-- To install this tool: `go get v.io/x/devtools/bendroid`
-
-
-## [Usage](https://godoc.org/v.io/x/devtools/bendroid)
-```
-export NDK_TOOLCHAIN=<path where the NDK toolchain has been unpacked>
-CC=${NDK_TOOLCHAIN}/arm-21/bin/arm-linux-androideabi-gcc \
-CXX=${NDK_TOOLCHAIN}/arm-21/bin/arm-linux-androideabi-g++ \
-${GOPATH}/bin/bendroid --help
-```
-
-## [Vanadium contributors](http://vanadium.github.io/community/contributing.html)
-
-All the above requirements are taken care of by the `android` profile.
-
-```
-jiri v23-profile install android
-jiri go install v.io/x/devtools/bendroid
-jiri run --target arm-android ${JIRI_ROOT}/release/go/bin/bendroid --help
-```
-
+bendroid is a go test executor for running tests/benchmarks on Android.
+See [documentation](https://godoc.org/v.io/x/devtools/bendroid) for details.
--- bendroid/doc.go
+++ bendroid/doc.go
@@ -6,61 +6,41 @@
 // DO NOT UPDATE MANUALLY
 
 /*
-bendroid attempts to emulate the behavior of go test, but running the tests and
-benchmarks on an android device.
+bendroid executes Go tests and benchmarks on an android device.
 
-Note that currently we support only a small subset of the flags allowed to 'go
-test'.
+It requires that 'adb' (the Android Debug Bridge:
+http://developer.android.com/tools/help/adb.html) be available in PATH.
 
-We depend on gradle and adb, so those tools should be in your path.
+Sample usage:
+  GOARCH=arm GOOS=android go test -exec bendroid crypto/sha256
+Or, alternatively:
+  GOARCH=arm GOOS=android go test -c cryto/sha256
+  bendroid ./sha256.test
 
-You should also set relevant CGO environment variables (for example pointing at
-the ndk gcc and g++) see: https://golang.org/cmd/cgo/.  Unlike gomobile, we
-don't set them for you. In particular, CC and CXX must be set to point to the
-binaries from the Android NDK toolchain. For example, if the toolchain is
-installed in $NDK_TOOLCHAIN, then:
-  export CC=${NDK_TOOLCHAIN}/arm-21/bin/arm-linux-androideabi-gcc
-  export CXX=${NDK_TOOLCHAIN}/arm-21/bin/arm-linux-androideabi-g++
-before running bendroid.
+Additionally, bendroid outputs a preamble of the form:
+  BENDROID_<variable_name>=<value>
+that describe the characteristics of the connected android device.
 
-Finally, bendroid requires Go 1.6 or above (since that contains some changes to
-make the generated shared library compatible with Android SDK version 23).
+WARNING: As of March 2016, bendroid is unable to ensure synchronization between
+what the executed program prints on standard output and standard error. This
+should hopefully be resolved with the next release of adb.
 
 Usage:
-   bendroid [flags] [-c] [build and test flags] [packages] [flags for test binary]
+   bendroid [flags] <filename to execute> [<flags provided to 'go test'>]
+
+See 'go help run' for details
+
+The bendroid flags are:
+ -bendroid.v=false
+   verbose output: print adb commands executed by bendroid
+ -bendroid.work=false
+   print the name of the directory on the device where all data is copied and do
+   not erase it
 
 The global flags are:
- -bench=
-   Run benchmarks matching the regular expression. By default, no benchmarks
-   run. To run all benchmarks, use '-bench .' or '-bench=.'.
- -benchmem=false
-   Print memory allocation statistics for benchmarks.
- -benchtime=1s
-   Print memory allocation statistics for benchmarks.
- -c=false
-   Compile the test binary to pkg.test but do not run it (where pkg is the last
-   element of the package's import path). The file name can be changed with the
-   -o flag.
  -metadata=<just specify -metadata to activate>
    Displays metadata for the program and exits.
- -o=
-   Compile the test binary to the named file. The test still runs (unless -c is
-   specified).
- -run=
-   Run only those tests and examples matching the regular expression.
- -tags=
-   a list of build tags to consider satisfied during the build. For more
-   information about build tags, see the description of build constraints in the
-   documentation for the go/build package.
  -time=false
    Dump timing information to stderr before exiting the program.
- -timeout=10m0s
-   If a test runs longer than t, panic.  The default is 10 minutes (10m).
- -v=false
-   Verbose output: log all tests as they are run. Also print all text from Log
-   and Logf calls even if the test succeeds.
- -work=false
-   print the name of the temporary work directory and do not delete it when
-   exiting.
 */
 package main
